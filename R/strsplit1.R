cf39b62b891b015a5e083d31a528fbf397cbaa79
rjimenezdata <rubensusername@gmail.com>
2026-05-19 16:00:43 -0700

    change str split

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,14 +1,20 @@
 
 #' Split a string by pattern.
 #'
-#' @param x A character string to split
-#' @param split A character pattern to split on.
+#' @param string A character string to split
+#' @param pattern A character pattern to split on.
+#' @param n A number of pieces to return. Defaults to Inf.
 #'
 #' @returns A character vector.
 #' @export
 #'
 #' @examples
-#' strsplit1("a,b,c", ",")
-strsplit1 <- function(x, split) {
-  strsplit(x, split = split)[[1]]
+#' str_split_one("a,b,c", ",")
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
 }
