0d43ab153d919ad1d89ecfc4f272bbf860ba6ab6
rjimenezdata <rubensusername@gmail.com>
2026-05-19 16:37:23 -0700

    New function

---
+++ R/str_split_comma.R
@@ -0,0 +1,24 @@
+#' Split a string on commas
+#'
+#' @param string A character vector with, at most, one element.
+#' @param n Maximum number of pieces to return. Default is Inf.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+#'
+#' y <- "a,b,c"
+#' str_split_one(y)
+str_split_comma <- function(string, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = ",", n = n)[[1]]
+  } else {
+    character()
+  }
+}
