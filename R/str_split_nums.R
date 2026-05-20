d1f75a42fcb03468a3b15831dbc706e2e2a5a572
rjimenezdata <rubensusername@gmail.com>
2026-05-19 19:52:20 -0700

    str_split_nums

--- R/str_split_nums.R
+++ R/str_split_nums.R
@@ -10,13 +10,16 @@
 #' str_split_nums("1,2,3", pattern = ",")
 #' str_split_nums("a,2,3", pattern = ",")
 str_split_nums <- function(string, pattern, n = Inf) {
-  pieces <- stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    pieces <- stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
 
-  ifelse(
-    stringr::str_detect(pieces, "^[-]?[0-9]*\\.?[0-9]+$"),
-    as.numeric(pieces),
-    NA
-  )
-} else {
-  numeric()
+    ifelse(
+      stringr::str_detect(pieces, "^[-]?[0-9]*\\.?[0-9]+$"),
+      as.numeric(pieces),
+      NA
+    )
+  } else {
+    numeric()
+  }
 }
