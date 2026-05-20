3d09c5a3fad04ea3df94d7ff8da32d3a611db73b
rjimenezdata <rubensusername@gmail.com>
2026-05-19 19:49:42 -0700

    Check for NA output

--- R/str_split_nums.R
+++ R/str_split_nums.R
@@ -10,10 +10,13 @@
 #' str_split_nums("1,2,3", pattern = ",")
 #' str_split_nums("a,2,3", pattern = ",")
 str_split_nums <- function(string, pattern, n = Inf) {
-  stopifnot(is.character(string), length(string) <= 1)
-  if (length(string) == 1) {
-    as.numeric(stringr::str_split(string = string, pattern = pattern, n = n)[[1]])
-  } else {
-    numeric()
-  }
+  pieces <- stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+
+  ifelse(
+    stringr::str_detect(pieces, "^[-]?[0-9]*\\.?[0-9]+$"),
+    as.numeric(pieces),
+    NA
+  )
+} else {
+  numeric()
 }
